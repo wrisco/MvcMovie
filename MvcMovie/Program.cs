0761a237c5c377b4fbd1751b62beabbf8247a587
Scott Wright <wrightsc@byui.edu>
2018-06-30 21:28:50 -0600

    First Commt

--- MvcMovie/Program.cs
+++ MvcMovie/Program.cs
@@ -1,25 +1,68 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.IO;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using Microsoft.AspNetCore;
+//using Microsoft.AspNetCore.Hosting;
+//using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.Logging;
+
+//namespace MvcMovie
+//{
+//    public class Program
+//    {
+//        public static void Main(string[] args)
+//        {
+//            BuildWebHost(args).Run();
+//        }
+
+//        public static IWebHost BuildWebHost(string[] args) =>
+//            WebHost.CreateDefaultBuilder(args)
+//                .UseStartup<Startup>()
+//                .Build();
+//    }
+//}
+
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using System;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+using MvcMovie;
 
 namespace MvcMovie
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
-        }
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var host = CreateWebHostBuilder(args).Build();
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
-    }
+			using (var scope = host.Services.CreateScope())
+			{
+				var services = scope.ServiceProvider;
+
+				try
+				{
+					var context = services.GetRequiredService<MvcMovieContext>();
+					context.Database.Migrate();
+					SeedData.Initialize(services);
+				}
+				catch (Exception ex)
+				{
+					var logger = services.GetRequiredService<ILogger<Program>>();
+					logger.LogError(ex, "An error occurred seeding the DB.");
+				}
+			}
+
+			host.Run();
+		}
+
+		public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+			WebHost.CreateDefaultBuilder(args)
+				.UseStartup<Startup>();
+	}
 }
